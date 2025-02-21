3a46b1ec79b80b8faf89350dc3da8926ec947c87
PriyanshiPrateek812 <154115675+PriyanshiPrateek812@users.noreply.github.com>
2025-02-21 09:41:26 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import Test from "./components/ClassEvent";
+import Header from "./components/ClassEvent";
+
+function App() {
+
+  return (
+    <div className="container">
+      <Header />
+        <Test />
+    </div>
+  );
+}
+
+export default App;
